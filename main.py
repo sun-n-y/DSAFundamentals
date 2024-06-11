c0e2f1b673232e36b30f32736f71ac38116b21f4
sunny patel <sunnykpatel.skp@gmail.com>
2024-06-11 12:58:26 -0400

    add completed lesson

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-arr = ['a', 'b', 'c']
-print(' '.join(arr))
+class MyClass:
+    # constructor
+    # self is passed in every method of a class, like this keyword
+        def __init__(self, nums):
+            # create member variables
+            self.nums = nums
+            self.size = len(nums)
+
+        # method for this class
+        # self keyword required as param, to get access to member variables
+        def getLenght(self):
+            return self.size
+
+        # call member function from another function
+        def getDoubleLenght(self):
+            return 2 * self.getLenght()
+
+p1 = MyClass([1,2,3])
+
+print(p1.getDoubleLenght())
+
+
 

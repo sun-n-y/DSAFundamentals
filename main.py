8239fab83a8ba89db373406f6aac7efcbcbff896
sunny patel <sunnykpatel.skp@gmail.com>
2024-06-10 21:36:17 -0400

    add lists lesson

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-import  math
-
-print(math.floor(3.2)) # 1
-print(math.ceil(3 / 2)) # 2
-print(math.sqrt(4)) # 4
-print(math.pow(10,2)) # 100
-
-print(math.pow(2, 200)) # large number
-print(math.pow(2,200) < float('inf')) # but still less than infinity
+arr = ['a', 'b', 'c']
+print(' '.join(arr))
 
